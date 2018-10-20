f5638a219c323ce7b399bd042d3c40f015e495f9
vmurashev <vitaly.murashev@gmail.com>
2018-10-20 21:51:44 +0300

    mingw-win32

--- minibuild.mk
+++ minibuild.mk
@@ -2,6 +2,7 @@
 
 #pragma os:windows nasm executable='C:\NASM\nasm.exe'
 
+#pragma os:windows toolset            module=mingw    package='C:\mingw-w64\i686-5.4.0-win32-dwarf-rt_v5-rev0\mingw32' arch=x86
 #pragma os:windows toolset required=1 module=mingw    package='C:\mingw-w64\x86_64-5.4.0-win32-seh-rt_v5-rev0\mingw64' arch=x86_64
 #pragma os:linux   toolset required=1 module=xtools   package='~/x-tools/x86_64-unknown-linux-gnu' prefix=x86_64-unknown-linux-gnu- arch=x86_64
 #pragma os:macosx  toolset required=1 module=clang    arch=x86_64:10.9
